a71cd3ce7f8dd49e8c0c7173e1e5fca190d0ac21
yangqiong <yangqionggo@163.com>
2022-06-08 15:32:36 +0800

    #03: Chains & Checks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use blockchainlib::*;
 
 fn main() {
-    let difficulty = 0x000fffffffffffffffffffffffffffff;
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned(), difficulty);
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
 
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
+    let mut block = Block::new(
+        0,
+        now(),
+        vec![0; 32],
+        0,
+        "Genesis block".to_owned(),
+        difficulty,
+    );
 
     block.mine();
+    println!("Mined genesis block {:?}", &block);
 
-    println!("{:?}", &block);
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            0,
+            "Another block".to_owned(),
+            difficulty,
+        );
+
+        block.mine();
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
+    }
 }
